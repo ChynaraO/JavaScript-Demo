f3d2eebc3f36fe3a33f642473afac9226f62b10b
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-04 22:05:24 -0600

    Primitive Tpes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // comment
-    const interestRate = 0.3;
-    interestRate = 1;
-    console.log(interestRate);
+
+let name = 'Mosh';
+let age = 30;
+let isApproved = true;
+let firstName = undefined;
+let lastName = null;  
