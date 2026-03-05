61b8e069c0aaec8e114cc67bb7165f39d9d8eaba
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-04 22:32:35 -0600

    Types of Functions

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-// comment
+// performing a task
 
 function greet(name, lastname){
     console.log('Hello ' + name + ' ' + lastname);
 }
+
+//calculates a value
+
+function square(number){
+    return number * number;
+}
+
+let number = square (2);
+console.log(number);
 
 greet('John', 'Doe');
 greet('Chynara');
