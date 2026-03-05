0614e38229873d4f218d334b1868bb36c5ff8c63
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-05 14:32:14 -0600

    Ternary Opertors

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 
-//Equality Operators
-
-//Strict equality operator(same type same value)
-let x = 1;
-console.log(x === 1);
-console.log(x !== 1);
-console.log('1' === 1);
-
-//Loose equality
-console.log(x == 1);
-console.log('1' == 1); //autoamtically converts to String
-console.log(true == 1); //autoamtically converts to boolean
+//Ternary Operators
+let points = 20;
+let type = points > 100 ? 'gold' : 'silver';
+console.log(type);
