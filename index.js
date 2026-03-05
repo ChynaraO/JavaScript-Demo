8c69d967d9b05f866bd15909b3a5e987e945a284
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-05 14:04:36 -0600

    Equality Operators

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-//Comparison operators
 
+//Equality Operators
+
+//Strict equality operator(same type same value)
 let x = 1;
-
-//Relational operators
-console.log(x > 0);
-console.log(x >= 0);
-console.log(x <= 0);
-console.log(x <= 0);
-
-//Equality
 console.log(x === 1);
 console.log(x !== 1);
+console.log('1' === 1);
 
+//Loose equality
+console.log(x == 1);
+console.log('1' == 1); //autoamtically converts to String
+console.log(true == 1); //autoamtically converts to boolean
