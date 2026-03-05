3ba62ee628a14c0edb3f56269b162afb88486ad9
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-04 22:01:35 -0600

    Constants

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 // comment
-    console.log('hello world!!!');
-    let name = 'Mosh';
-    //cannot be reserved keywords
-    //meaningful
-    //cannot start with a number
-    //cannot contain space or highen
-    //are case sensitive
-
-
-    let firstName = 'Mosh';
-    let lastNamr = 'Omur';
-    let FirstName;
-    console.log(name);
+    const interestRate = 0.3;
+    interestRate = 1;
+    console.log(interestRate);
