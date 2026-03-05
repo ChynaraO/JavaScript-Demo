9a78e2b2eddafe37fd32e9b49c56c73b7ccf8359
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-05 15:06:45 -0600

    Bitwise Operators

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 
-//Logical Operators with Non-booleans
+//Bitwise Operators
 
-//Falsy : 
-// undefined
-//0
-//null
-//''
-//NaN - not a number
+// 1 = 00000001
+// 2 = 00000010
+// R = 00000011
 
-let userColor = 'red';
-userColor = undefined;
-let defaultColor = 'blue';
-let currentColor = userColor || defaultColor;
+// 8 = 00001000
+// 2 = 00000010
+// R = 00001010
 
-console.log(currentColor);
+console.log(1 | 2); //bitwise or. 3
+console.log(8 & 2); // bitwise &. 0
+
+//Read, Write, Execute
+// 00000001
+// 00000010
+// 00000100
+
+const readPermition = 4;
+const writePermition = 2;
+const executePermition = 1;
+
+let myPermition = 0;
+myPermition = myPermition | writePermition;
+console.log(myPermition); //6 bitwise
+
+let message = 
+(myPermition & readPermition) ? 'yes' : 'no';
+console.log(message);
 
 
