096ab83c5640c42a9da1ca03e465fa4be66bbc8b
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-04 22:12:35 -0600

    Dynamic Typing

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // comment
 
-let name = 'Mosh';
-let age = 30;
-let isApproved = true;
-let firstName = undefined;
-let lastName = null;  
+let name = 'Mosh'; //typeOf String
+let age = 30;      //typeOf number
+age = 30.1;        //typeOf number
+age = 'thirty';
+let isApproved = true;  //typeOf boolean
+let firstName = undefined;  //typeOf undefined
+let lastName = null;        //typeOf Object
