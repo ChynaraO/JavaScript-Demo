da20c48d5d2e6e4f819ea4bca6954f0194d02cf5
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-04 21:55:15 -0600

    Variables

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // comment
     console.log('hello world!!!');
-    
+    let name = 'Mosh';
+    //cannot be reserved keywords
+    //meaningful
+    //cannot start with a number
+    //cannot contain space or highen
+    //are case sensitive
+
+
+    let firstName = 'Mosh';
+    let lastNamr = 'Omur';
+    let FirstName;
+    console.log(name);
