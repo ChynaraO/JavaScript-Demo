d8097118bd1dc82b4a2a4262603d02c8899d8336
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-06 10:23:20 -0600

    For loop

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 
-//Bitwise Operators
+// For loop
 
-// 1 = 00000001
-// 2 = 00000010
-// R = 00000011
-
-// 8 = 00001000
-// 2 = 00000010
-// R = 00001010
-
-console.log(1 | 2); //bitwise or. 3
-console.log(8 & 2); // bitwise &. 0
-
-//Read, Write, Execute
-// 00000001
-// 00000010
-// 00000100
-
-let role = 'gues';
-
-switch(role){
-    case 'guest':
-        console.log('Guest User');
-        break;
-    
-    case 'moderator':
-        console.log('Moderator User');
-        break;
-
-    default :
-    console.log("Unknown User");
+for(let i = 0; i < 5; i++){
+    console.log('Hello World', i+1);
 }
 
+//odd numbers
+for(let i = 1; i <= 5; i++){
+    if(i % 2 !== 0) console.log(i);
+}
