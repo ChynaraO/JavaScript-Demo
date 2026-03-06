45fb106ee0afa57c6697712c7af50bcdc020a0fd
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-06 10:25:44 -0600

    While loop

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
-// For loop
 
-for(let i = 0; i < 5; i++){
-    console.log('Hello World', i+1);
-}
-
-//odd numbers
 for(let i = 1; i <= 5; i++){
     if(i % 2 !== 0) console.log(i);
+}
+
+// While loop
+let i = 0;
+while(i <= 5){
+    if(i !== 0) console.log(i);
+    i++;
 }
