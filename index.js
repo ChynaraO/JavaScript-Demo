16897e0a5030e4cc9feed0215f74691cfb208328
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-06 12:17:28 -0600

    Do-While Loop

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ for(let i = 1; i <= 5; i++){
     if(i % 2 !== 0) console.log(i);
 }
 
-// While loop
-let i = 0;
-while(i <= 5){
+// // While loop
+let i = 9;
+// while(i <= 5){
+//     if(i !== 0) console.log(i);
+//     i++;
+// }
+
+//do-while
+do{
     if(i !== 0) console.log(i);
     i++;
-}
+} while (i <= 5);
