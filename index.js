f46463b63024a1a81f85902685c322d8621a1ef2
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-06 12:54:54 -0600

    Landscape or Portrait

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-let number = max(5,3);
-console.log(number);
-
-// function max(number1, number2){
-//     if(number1>number2) return number1;
-//     else return number2;
-// }
-
-function max(number1, number2){
-    return (number1>number2) ? number1 : number2;
+function isLandscape (width, hight){
+    // return (width > hight) ? true : false;
+    return (width > hight);
 }
+console.log(isLandscape(70, 50));
