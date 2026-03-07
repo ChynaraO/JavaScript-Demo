4e27d8a1086267065d6f8fcc20933d58d5a20d82
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-07 06:42:11 -0600

    Dynamic Nature of Objects

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-//Factory functions
-//Camel Notation: oneTwo
-//Pascal Notation: OneTwo
-function createCircle(raduis){
-    return {
-        raduis,
-        draw(){
-            console.log('draw');
-        }
-    };
+const circle = {
+    radius: 1
 }
- const circle1 = createCircle(1);
- console.log(circle1);
 
- const circle2 = createCircle(2);
- console.log(circle2);
+circle.color = 'yellow';
+circle.draw = function(){}
 
- //constructor function
- function Circle(radius){
-    this.radius = radius;
-    this.draw = function(){
-        console.log('dddd');
-    }
- }
+delete circle.color;
 
- const circle = new Circle(4);
- console.log(circle);
- circle.draw();
+console.log(circle);
