2566860744e2d2a72f72462f3acf5caf25a5cdec
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-07 16:15:44 -0600

    Functions are Objects

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const circle = {
-    radius: 1
+function Circle(radius){
+    this.radius = radius,
+    this.draw = function(){
+        console.log('draw');
+    }
 }
 
-circle.color = 'yellow';
-circle.draw = function(){}
 
-delete circle.color;
+// const Circle1 = new Function('raduis', `
+//     this.radius = radius,
+//     this.draw = function(){
+//         console.log('draw');
+//     }
+//         `
+// );
 
-console.log(circle);
+console.log(Circle);
+Circle.call({}, 1);
+Circle.apply({}, [1,2,3]);
+
+
+const another = new Circle(1);
+console.log(another);
