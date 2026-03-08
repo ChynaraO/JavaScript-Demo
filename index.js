32c77318b369339a824dc7958a45b2a12ec98463
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-07 19:14:01 -0600

    Object Equality

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-//street
-//city
-//zipCode
-//showAddress(address);
-
-let address = {
-    stree: 'a',
-    city: 'b',
-    zipCode: 'c'
-};
-
-//Factory function
-function createAddress(street, city, zipCode){
-    return {street, city, zipCode};
-}
-let address1 = createAddress('a', 'n', 'f');
-console.log(address1);
 
 //Constructor
 function Address(street, city, zipCode){
@@ -22,5 +5,20 @@ function Address(street, city, zipCode){
     this.city = city;
     this.zipCode =zipCode
 }
+let address1 = new Address('a', 'f', 'd');
 let address2 = new Address('a', 'f', 'd');
 console.log(address2);
+
+function areEqual (address1, address2){
+    // === if these objects's properties reference are same
+return address1.street === address2.street &&
+address1.city === address2.city &&
+address1.zipCode === address2.zipCode
+}
+
+function areSame(address1, address2){
+    // === if these objects's reference are same
+    return address1 === address2;
+}
+console.log(areSame(address1, address2));
+console.log(areEqual(address1, address2));
