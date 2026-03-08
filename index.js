52b2fe16bf2d8d6ce5b26dae253d8f5f0fb91361
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-07 19:20:51 -0600

    Blog Post Object

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
+//blog post object
 
-//Constructor
-function Address(street, city, zipCode){
-    this.street = street,
-    this.city = city;
-    this.zipCode =zipCode
-}
-let address1 = new Address('a', 'f', 'd');
-let address2 = new Address('a', 'f', 'd');
-console.log(address2);
+//title
+//body
+//author
+//views
+//cmments
+//(author, body)
+//isLive
 
-function areEqual (address1, address2){
-    // === if these objects's properties reference are same
-return address1.street === address2.street &&
-address1.city === address2.city &&
-address1.zipCode === address2.zipCode
-}
+let post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    viewa: 10,
+    comments: [
+        {author: 'a', body: 'b'},
+        {author: 'c', body: 'd'}
+    ],
+    isLive: true
+};
 
-function areSame(address1, address2){
-    // === if these objects's reference are same
-    return address1 === address2;
-}
-console.log(areSame(address1, address2));
-console.log(areEqual(address1, address2));
+console.log(post);
