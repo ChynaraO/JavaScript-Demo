76e2ca40f9e3f84e74e3aad42bc9e60e5fa03e20
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-07 19:43:47 -0600

    Finding Elements (Primitives) in an Array

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const numbers = [3, 4];
+const numbers = [1,2,3, 1, 4];
+console.log(numbers.indexOf(2));
+console.log(numbers.indexOf('5'));
+console.log(numbers.lastIndexOf(1));
+console.log(numbers.indexOf(1), 2);
 
-//add an element 
-//End
-numbers.push(5,6);
+//old
+console.log(numbers.indexOf(1) !== -1);
 
-//Beginning
-numbers.unshift(1,2);
-
-//Middle
-numbers.splice(2, 0, 'a', 'b');
-console.log(numbers);
+//new
+console.log(numbers.includes(1));
