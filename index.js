4c2e661f3be52e8575e1d7f5f8e0fc4a6ccd3745
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-07 19:25:11 -0600

    Constructor functions

--- index.js
+++ index.js
@@ -8,16 +8,25 @@
 //(author, body)
 //isLive
 
-let post = {
-    title: 'a',
-    body: 'b',
-    author: 'c',
-    viewa: 10,
-    comments: [
-        {author: 'a', body: 'b'},
-        {author: 'c', body: 'd'}
-    ],
-    isLive: true
-};
+// let post = {
+//     title: 'a',
+//     body: 'b',
+//     author: 'c',
+//     viewa: 10,
+//     comments: [
+//         {author: 'a', body: 'b'},
+//         {author: 'c', body: 'd'}
+//     ],
+//     isLive: true
+// };
 
+function Post(title, body, author){
+    this.title = title;
+    this.body = body;
+    this.author= author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+}
+post = new Post('a', 'g', 'k');
 console.log(post);
