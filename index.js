aba81e200c9afd4d29282cd106ad8114c6b4a4c9
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-09 10:40:06 -0500

    Emtying an Array

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const numbers = [1,2,3,4,5,6];
-//End
-const last = numbers.pop();
-console.log(last);
-// //beginning
-const first = numbers.shift()
-console.log(first);
-// //middle
-numbers.splice(2,1);
+let numbers = [1,2,3,4,5,6];
+
+//Emtying an Array only if it has the only reference
+//Option 1
+numbers = [];
+
+// Option 2
+numbers.length = 0;
+
+//Option 3
+numbers.splice(0, numbers.length);
+
+//Option 4
+while(numbers.length > 0)
+    numbers.pop();
+
+
 console.log(numbers);
