e040483f71117ab00768bf38a0e703102d5f42c1
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-09 10:21:34 -0500

    Arrow function

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const cources = [
     {id: 2, name: 'b'}, 
 ];
 
-// console.log(cources.includes({id: 1, name: 'a'}));
+//Arrow function syntax
 
-const course = cources.find(function(course){
-    return course.name === 'a';
-});
+const course = cources.find(course => course.name === 'a');
+
+
 const course1 = cources.find(function(course){
     return course.name === 'as';
 });
