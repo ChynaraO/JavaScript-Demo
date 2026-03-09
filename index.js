5cc2f62862d58df76c483bcc0101323568280c6a
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-09 13:17:44 -0500

    Joining Arrays

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const first = [1,2,3,4];
 const second = [5,6,7];
 
-for(let number of first)
-    console.log(number);
+const joined = first.join(',');
+console.log(joined);
 
-first.forEach((number, index)=>
-    console.log(index, number));
+const message= 'this is my first message';
+const parts = message.split(' ');
+console.log(parts);
 
+const combined = parts.join('-');
+console.log(combined);
 
