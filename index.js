d2a6c4217ed80a377f8d9d6d1268dec4ea4cb759
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-10 17:01:10 -0500

    Mapping an Array

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const numbers = [1,2,-1,3,4];
 
-const filtered = numbers.filter(n => n >=0);
-console.log(filtered);
+// const filtered = numbers.filter(n => n >=0);
+// const items = filtered.map(n => '<li>' + n + '</li>')
+// console.log(filtered);
+// console.log(items);
+// const html = '<ul>' + items.join('') + '</ul>';
+// console.log(html);
+const items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({value: n}))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value);
+
+console.log(items);
