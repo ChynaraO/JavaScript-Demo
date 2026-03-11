beb95e6c76e075b28b3245c49fbef1831da6e6f1
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-11 07:35:38 -0500

    Count occurences

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const numbers = [1,2,3,4];
+const numbers = [1,2,3,1, 4];
 
-//array , index of an element, offset -position where it needs to be moved
-const output = move(numbers, 0, -2);
-console.log(output);
-console.log(move(numbers, 0, -1));
+const count = countOccurences(numbers, 1);
+console.log(count);
+//basic
+// function countOccurences(array, searchElement){
+//     let count = 0;
+//     for(let element of array)
+//         if(element === searchElement)
+//             count++;
+//     return count;
+// }
 
-
-function move(array, index, offset){
-    const position = index + offset;
-    if(position >= array.length || position < 0){
-        console.error('Invalid offset');
-    }
- 
-    const output = [...array];
-    const element = output.splice(index, 1)[0];
-    output.splice(index + offset, 0, element);
-    return output;
+//reduce
+function countOccurences(array, searchElement){
+    // let count = 0;
+    // for(let element of array)
+    //     if(element === searchElement)
+    //         count++;
+    // return count;
+return array.reduce((accumulator, current) => {
+        const occurence = (current === searchElement) ? 1 : 0;
+        console.log(accumulator, current, searchElement);
+        return accumulator + occurence;
+    }, 0);
 }
+
