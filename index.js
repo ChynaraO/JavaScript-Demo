f42804a08c2757db583c0c97c7e467335d523b31
ChynaraO <omurzakova_chyna@mail.ru>
2026-03-11 07:27:41 -0500

    Moving an Element

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const numbers = [1,2,3,4];
 
-const output = except(numbers, [1,2]);
-
+//array , index of an element, offset -position where it needs to be moved
+const output = move(numbers, 0, -2);
 console.log(output);
-function except(array, excuded){
-    const output = [];
-    for(let element of array)
-        if(!excuded.includes(element))
-            output.push(element);
+console.log(move(numbers, 0, -1));
+
+
+function move(array, index, offset){
+    const position = index + offset;
+    if(position >= array.length || position < 0){
+        console.error('Invalid offset');
+    }
+ 
+    const output = [...array];
+    const element = output.splice(index, 1)[0];
+    output.splice(index + offset, 0, element);
     return output;
 }
